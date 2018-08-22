f9451ad8381d0207afd982676cf751531acdf7b2
Pawel <tester1464@gmail.com>
2018-08-22 14:01:52 +0200

    Add project files.

---
+++ FormUI/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormUI
+{
+    class Person
+    {
+        public int id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+    }
+}
